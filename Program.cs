6842636b67e1b47ee3d02debe82bd82f93b51e76
Vladimir <v0vanchikanna@gmail.com>
2022-09-08 17:31:48 +0300

    Z1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write($"Введи число М(количество чисел): ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[m];
+
+void InputNumbers(int m){
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Введи {i+1} число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+int MoreZero(int[] a)
+{
+  int count = 0;
+  for (int i = 0; i < a.Length; i++)
+  {
+    if(array[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+InputNumbers(m);
+Console.WriteLine($"Введено чисел > 0: {MoreZero(array)} ");
